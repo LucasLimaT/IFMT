97934cc4c37fbda5178e71124d06a54b71bd7e22
Lucas Taveira <140752422+LucasLimaT@users.noreply.github.com>
2023-08-28 20:20:44 +0000

    feat: adicionadas novas funcionalidades em doublelist.c

--- projetos/estruturas/DoubleList/doublelist.c
+++ projetos/estruturas/DoubleList/doublelist.c
@@ -1,4 +1,5 @@
-#include "doublelist.h";
+#include "doublelist.h"
+#include "log.h"
 
 void init(DoublyLinkedList *list) {
     log_info("Inicializando lista");
@@ -8,17 +9,44 @@ void init(DoublyLinkedList *list) {
     trash->next = trash;
     trash->data = NULL;
     list->first = trash;
+    list->size = 0;
     log_trace("init <-");
 }
 
 bool isEmpty(DoublyLinkedList *list) {
     log_info("Verificando se a lista está vazia");
     log_trace("isEmpty ->");
-    
+    log_debug("list->size: (%p)", list->size);
+    log_trace("isEmpty <-");
+    return (list->size == 0);
 }
 
 int enqueue(DoublyLinkedList *list, void *data) {
-
+    log_info("Enfileirando novo elemento!");
+    log_trace("enqueue ->");
+    Node *No = (Node *)malloc(sizeof(Node));
+    Node *aux = list->next;
+    if(isEmpty(list)) {
+        No->previous = aux;
+        aux = No;
+        No->data = data;
+        No->next = NULL;
+        list->size += 1;
+        log_debug("No(No->next, No->previous, No->data): %p(%p, %p, %p)", No, No->previous, No->next, No->data);
+        log_trace("enqueue <-");
+        return data;
+    }
+    while (aux->next != NULL) {
+        aux = aux->next;
+    }
+    No->data = data;
+    No->next = NULL;
+    No->previous = aux;
+    aux->next = No;
+    log_debug("No(No->data, No->next, No->previous)", No, No->data, No->next, No->previous);
+    list->size += 1;
+    log_trace("enqueue <-");
+    return data;
 }
 
 void* dequeue(DoublyLinkedList *list);
