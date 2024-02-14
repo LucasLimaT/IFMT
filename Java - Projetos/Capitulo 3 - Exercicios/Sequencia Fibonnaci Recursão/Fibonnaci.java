6ec14d39d3bc1227e21ea5099c43bf29343873ed
LucasLimaT <lucas.taveira@estudante.ifmt.edu.br>
2024-02-14 06:05:54 -0400

    Atualização: Fibonnaci feito por operador condicional ternario

--- Java - Projetos/Capitulo 3 - Exercicios/Sequencia Fibonnaci Recursão/Fibonnaci.java
+++ Java - Projetos/Capitulo 3 - Exercicios/Sequencia Fibonnaci Recursão/Fibonnaci.java
@@ -1,14 +1,9 @@
 class Fibonnaci {
     int a = 1;
     int b = 0;
+    int x;
    
     int calculaFibonnaci(int numero) {
-        if(numero%2 == 0){
-            b += a;
-            return b;
-        } else {
-            a += b;
-            return a;
-        }
+        return x = (numero%2 == 0) ? (b += a) & (x = b): (a += b) & (x = a);
     }
 }
